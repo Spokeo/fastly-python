169c4bfdae0962ad2ecacc76f51c61521aeef1cf
Arthur Freyman <afreyman@spokeo.com>
2015-09-30 15:21:39 -0700

    Added some log output. Fixed a conditional

--- bin/fastly_tool.py
+++ bin/fastly_tool.py
@@ -31,29 +31,34 @@ def main():
     def get_latest(version):
         latest_version = version.pop()
         if latest_version.locked is True or latest_version.active is True:
-            print "\n[ Cloning version %d ]\n"\
-                % (latest_version.number)
+            print "\n[ Cloning version {0} ]\n".format(latest_version.number)
             latest_version = client.clone_version(service.id, latest_version.number)
+        print "\n[ The config version is going to be {0} ]\n".format(latest_version.number)
         return latest_version
 
     def upload_vcl(content):
         if vcl_name in latest.vcls:
             client.update_vcl(service.id, latest.number, vcl_name, content=vcl_content)
+            print "\n[ Uploading VCL {0} ]\n".format(vcl_name)
         else:
             client.upload_vcl(service.id, latest.number, vcl_name, vcl_content)
+            print "\n[ Uploading VCL {0} ]\n".format(vcl_name)
         client.activate_version(service.id, latest.number)
+        print "\n[ Activating version {0} ]\n".format(latest.number)
 
     def delete_vcl():
         vcls = client.list_vcls(service.id, latest.number)
         for vcl in vcls:
             client.delete_vcl(service.id, latest.number, vcl.name)
         client.activate_version(service.id, latest.number)
+        print "\n[ Activating version {0} ]\n".format(latest.number)
 
 
     latest = get_latest(versions)
-    if args.action is 'add':
+    if 'add' in args.action:
         upload_vcl(vcl_content)
     else:
+        print "\n[ Deleting VCL {0} ]\n".format(vcl_name)
         delete_vcl()
 
 
